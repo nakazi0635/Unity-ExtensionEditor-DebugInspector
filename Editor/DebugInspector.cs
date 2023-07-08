5ae41a71997fbccb56ec49d428937d57d83d090e
nakazi <nakazi1209@gmail.com>
2023-07-08 18:43:04 +0900

    update csfile

--- Editor/DebugInspector.cs
+++ Editor/DebugInspector.cs
@@ -12,17 +12,20 @@ public class DebugInspector : EditorWindow
     string search = string.Empty;
     // スクリプトのみを検索するかどうかを管理するフラグ
     bool searchScriptsOnly = false;
+    
 
     // メニュー欄に追加
     [MenuItem("Tools/Debug Inspector")]
     public static void ShowWindow()
     {
-        // エディターウィンドウを作成して表示し、そのインスタンスを返す
+        // エディターウィンドウを作成して表示し、そのインスタンスを返却
         EditorWindow.GetWindow(typeof(DebugInspector));
     }
 
     void OnGUI()
     {
+        GUIStyle textFieldStyle = new GUIStyle(EditorStyles.textField);
+        textFieldStyle.alignment = TextAnchor.MiddleLeft;
         // 水平スクロールの開始
         GUILayout.BeginHorizontal("HelpBox");
         // ラベルの表示
@@ -37,38 +40,47 @@ public class DebugInspector : EditorWindow
         // オブジェクトのリストスクロールの開始
         scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
 
+        // シーン内の全てのGameObjectに対して一つずつ処理を実行
         foreach (GameObject go in Resources.FindObjectsOfTypeAll(typeof(GameObject)))
         {
             // 検索文字列が含まれないオブジェクトはスキップ
             if (!string.IsNullOrEmpty(search) && !go.name.ToLower().Contains(search.ToLower()))
                 continue;
 
-            // "Scripts Only"が選択されていて、オブジェクトが自作スクリプトを持っていない場合はスキップ
+            // "Scripts Only"が選択されていて、オブジェクトが自作のスクリプトを持っていない場合はスキップ
             if (searchScriptsOnly && !HasUserScript(go))
                 continue;
 
             // 垂直スクロールの開始
             EditorGUILayout.BeginVertical("box");
 
+            //オブジェクト名を表示
             EditorGUILayout.LabelField("Object Name: " + go.name, EditorStyles.boldLabel);
+            //オブジェクトが持つ全てのコンポーネントを調査
             foreach (Component comp in go.GetComponents<Component>())
             {
                 // "Scripts Only"が選択されていて、コンポーネントが自作のスクリプトでない場合はスキップ
                 if (searchScriptsOnly && !(comp is MonoBehaviour) || (comp.GetType().Namespace != null && comp.GetType().Namespace.StartsWith("UnityEngine")))
                     continue;
+                //スプリクト名を表示
+                EditorGUILayout.LabelField("  Script Name: " + comp.GetType().Name, EditorStyles.boldLabel);
 
-                EditorGUILayout.LabelField("Script Name: " + comp.GetType().Name, EditorStyles.boldLabel);
-
+                // 現在のコンポーネントをSerializedObjectとして取得
                 SerializedObject so = new SerializedObject(comp);
+                // SerializedObjectからプロパティのイテレータを取得
                 SerializedProperty prop = so.GetIterator();
+                // すべてのプロパティを順番に処理
                 while (prop.NextVisible(true))
                 {
                     // m_Scriptのプロパティはスキップ
                     if (prop.name == "m_Script") continue;
 
+                    // 垂直スクロールの開始
                     EditorGUILayout.BeginHorizontal();
-                    EditorGUILayout.LabelField("Variable Name: " + prop.name);
-                    EditorGUILayout.LabelField("Value: " + GetPropertyValue(prop));
+                    // プロパティ名と値を表示
+                    EditorGUILayout.LabelField("    Variable Name: " + prop.name);
+                    EditorGUILayout.LabelField("    Value: " + GetPropertyValue(prop));
+                    // 垂直スクロールの終了
                     EditorGUILayout.EndHorizontal();
                 }
             }
@@ -88,10 +100,10 @@ public class DebugInspector : EditorWindow
             if (!(comp is MonoBehaviour) || (comp.GetType().Namespace != null && comp.GetType().Namespace.StartsWith("UnityEngine")))
                 continue;
             
-            return true; // 自作のスクリプトが見つかったら true を返す
+            return true; // 自作のスクリプトが見つかったらtrueを返却
         }
 
-        return false; // 自作のスクリプトが見つからなかったら false を返す
+        return false; // 自作のスクリプトが見つからなかったらfalseを返却
     }
 
     // SerializedProperty の値を文字列で取得するメソッド
@@ -109,11 +121,9 @@ public class DebugInspector : EditorWindow
                 return prop.stringValue;
             case SerializedPropertyType.Color:
                 return prop.colorValue.ToString();
-            case SerializedPropertyType.ObjectReference:
-                return prop.objectReferenceValue ? prop.objectReferenceValue.ToString() : "null";
             // 必要に応じて他の型も追加する
             default:
-                return "<unknown>"; // 未知の型は<unknown>を返す
+                return "<unknown>"; // 他の型は<unknown>を返却
         }
     }
 }
